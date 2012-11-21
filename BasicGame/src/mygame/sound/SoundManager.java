ade6cfd1922200a88cf8967820b5868c175890bf
nagash <nagash@nagash-desktop>
2012-11-21 12:19:56 +0100

    Cambiadas propiedades del nodo de audio para ahorrar memoria.

--- BasicGame/src/mygame/sound/SoundManager.java
+++ BasicGame/src/mygame/sound/SoundManager.java
@@ -44,8 +44,8 @@ public class SoundManager {
 
         // Initialize gothic tune
         AudioNode gothicTune = new AudioNode(assetManager,
-"Sounds/Songs/Dark_music_Vampirical.ogg",false);
-        gothicTune.setLooping(true);
+"Sounds/Songs/Dark_music_Vampirical.ogg",true);
+        //gothicTune.setLooping(true);
         gothicTune.setVolume(0.3f);
         gothicTune.setName("gothicTune");
         rootNode.attachChild(gothicTune);
